ecea7bf6efa037aea9ae0b8bd7b905c2932e026b
Johnny Domino <johnny@yayuhh.com>
2015-05-14 16:18:25 -0400

    implement example endpoints

---
+++ endpoints/example.js
@@ -0,0 +1,49 @@
+const Joi = require('joi');
+
+
+// SCHEMAS
+const EXPECTED_INCOMING_SCHEMA =
+Joi.object().keys(
+  {
+    user: Joi.object().keys({                  // required
+      name: Joi.string(),                      // optional
+      email: Joi.string().email().required()   // required
+    }).required()
+  }
+);
+
+const EXPECTED_OUTGOING_SCHEMA =
+Joi.object().keys(
+  {
+    bank: Joi.object().keys({                  // optional
+      balance: Joi.number().min(0).required(), // required
+      as_of: Joi.date().required(),            // required
+    })
+  }
+);
+
+
+// ENDPOINTS
+module.exports = [
+
+  // usage: curl -d 'user[email]=johnny@heroku.com' http://localhost:3000/example
+  {
+    method: 'POST',
+    path:   '/example',
+    config: {
+      validate: { payload: EXPECTED_INCOMING_SCHEMA },
+      response: { schema:  EXPECTED_OUTGOING_SCHEMA }
+    },
+    handler: function (request, reply) {
+      /* var db = request.server.app.db; */
+
+      reply({
+        bank: {
+          balance: 54.32,
+          as_of:   Date.now()
+        }
+      });
+    }
+  }
+
+];
---
+++ plugins/endpoints.js
@@ -0,0 +1,17 @@
+var info   = require('../package.json');
+var endpoints = require('../endpoints');
+
+exports.register = function (server, options, next) {
+
+  // Load up the endpoints
+  endpoints.forEach(function(r) {
+    server.route(r);
+  });
+
+  return next();
+};
+
+exports.register.attributes = {
+  name: 'endpoints',
+  version: info.version
+};
