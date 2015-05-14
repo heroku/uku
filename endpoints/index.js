de6091ede13840077b7a7eb8ec3741d5566f1ff3
Johnny Domino <johnny@yayuhh.com>
2015-05-14 16:18:25 -0400

    implement example endpoints

---
+++ endpoints/index.js
@@ -0,0 +1,18 @@
+const fs   = require('fs');
+const Path = require('path');
+const Hoek = require('hoek');
+
+const endpoints  = [];
+const exclude = ['helpers.js'];
+
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf('.') !== 0) && (file !== 'index.js') && (exclude.indexOf(file) < 0);
+  })
+  .forEach(function(file) {
+    const additionalEndpoints = require(Path.join(__dirname, file));
+    Hoek.merge(endpoints,additionalEndpoints);
+  });
+
+module.exports = endpoints;
