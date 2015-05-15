8c759f866b12cc3475b0805103593dbd04984f55
Johnny Domino <johnny@yayuhh.com>
2015-05-15 13:07:38 -0400

    inline validation schemas

--- endpoints/example.js
+++ endpoints/example.js
@@ -1,28 +1,6 @@
 const Joi = require('joi');
 
 
-// SCHEMAS
-const EXPECTED_INCOMING_SCHEMA =
-Joi.object().keys(
-  {
-    user: Joi.object().keys({                  // required
-      name: Joi.string(),                      // optional
-      email: Joi.string().email().required()   // required
-    }).required()
-  }
-);
-
-const EXPECTED_OUTGOING_SCHEMA =
-Joi.object().keys(
-  {
-    bank: Joi.object().keys({                  // optional
-      balance: Joi.number().min(0).required(), // required
-      asOf: Joi.date().iso().required()              // required
-    })
-  }
-);
-
-
 // ENDPOINTS
 module.exports = [
 
@@ -30,10 +8,27 @@ module.exports = [
   {
     method: 'POST',
     path: '/example',
+
     config: {
-      validate: { payload: EXPECTED_INCOMING_SCHEMA },
-      response: { schema: EXPECTED_OUTGOING_SCHEMA }
+      validate: {
+        payload: Joi.object().keys({                 // required
+          user: Joi.object().keys({                  // required
+            name: Joi.string(),                      // optional
+            email: Joi.string().email().required()   // required
+          }).required()
+        })
+      },
+
+      response: {
+        schema: Joi.object().keys({                  // optional
+          bank: Joi.object().keys({                  // optional
+            balance: Joi.number().min(0).required(), // required
+            asOf: Joi.date().iso().required()        // required
+          })
+        })
+      }
     },
+
     handler: function (request, reply) {
       /* const db = request.server.app.db; */
 
