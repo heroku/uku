ad9598f525d4d03f401bd151ba35769aefe32d08
Johnny Domino <johnny@yayuhh.com>
2015-05-15 16:13:11 -0400

    enforce https in production

---
+++ plugins/enforce-ssl.js
@@ -0,0 +1,25 @@
+const info = require('../package.json');
+const conf = require('../config');
+
+exports.register = function (server, options, next) {
+
+  // redirect all http request to secure route
+  if (conf.get('/server/enforceSSL')) {
+    server.ext('onRequest', function (request, reply) {
+
+      if (request.headers['x-forwarded-proto'] !== 'https') {
+        return reply('Forwarding to a more secure route')
+              .redirect('https://' + request.headers.host + request.path);
+      }
+
+      reply();
+    });
+  }
+
+  next();
+};
+
+exports.register.attributes = {
+  name: 'enforce-ssl',
+  version: info.version
+};
