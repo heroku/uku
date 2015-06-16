fa59bef37d07a1734e7cb60641eb31f0e0b53a74
Johnny Domino <jvdomino@gmail.com>
2015-06-16 13:22:37 -0700

    refactor to become a dependency of uku-template

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const pkg = require('./package.json');
-const server = require('./lib/server');
+const Path = require('path');
+const path = Path.join(process.cwd(), 'package.json');
+const pkg = require(path);
+const plugins =
+  Object
+    .keys(pkg.dependencies)
+    .filter((moduleName) => {
+      return moduleName.match(/^uku-/);
+    });
 
-server.start(printStatus);
 
-function printStatus() {
-  console.log(`${pkg.name} running at: ${server.info.uri}`);
+exports.server = require('./lib/server');
+
+exports.plugins = (require) => {
+  return plugins.map((moduleName) => {
+    console.log('Registering ', moduleName);
+    return require(moduleName);
+  });
+};
+
+exports.printStatus = function printStatus() {
+  console.log(`${pkg.name} running at: ${exports.server.info.uri}`);
+};
+
+exports.registerPlugins = function(require, done) {
+  // register all the plugins
+  exports.server.register(exports.plugins(require), (err) => {
+    done(err);
+  });
 }
