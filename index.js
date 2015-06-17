74bdeecbcd8e97d2cc3ad9d165d289eb82e17dd3
Johnny Domino <jvdomino@gmail.com>
2015-06-16 17:06:40 -0700

    defer plugin loading

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 const Path = require('path');
-const path = Path.join(process.cwd(), 'package.json');
-const pkg = require(path);
-const plugins =
-  Object
-    .keys(pkg.dependencies)
-    .filter((moduleName) => {
-      return moduleName.match(/^uku-/);
-    });
+const pkgPath = Path.join(process.cwd(), 'package.json');
+const pkg = require(pkgPath);
+const isRunFromCLI = (require.main === module);
 
+const pluginNames = Object.keys(pkg.dependencies)
+                          .filter((moduleName) => {
+                            return moduleName.match(/^uku-/);
+                          });
 
-exports.server = require('./lib/server');
+const server = exports.server = require('./lib/server');
 
-exports.plugins = (require) => {
-  return plugins.map((moduleName) => {
-    console.log('Registering ', moduleName);
+const pluginModules = (require) => {
+  return pluginNames.map((moduleName) => {
+    // $lab:coverage:off$
+    if (isRunFromCLI) {
+      console.log('registering uku plugin: ', moduleName);
+    }
+    // $lab:coverage:on$
+
     return require(moduleName);
   });
 };
 
 exports.printStatus = function printStatus() {
-  console.log(`${pkg.name} running at: ${exports.server.info.uri}`);
+  console.log(`${pkg.name} running at: ${server.info.uri}`);
 };
 
-exports.registerPlugins = function(require, done) {
+exports.registerPlugins = function(require, additionalPlugins, done) {
+  const pluginsPath = Path.join(__dirname, 'lib/plugins');
+  const plugins = pluginModules(require).concat(require(pluginsPath)).concat(additionalPlugins);
+
   // register all the plugins
-  exports.server.register(exports.plugins(require), (err) => {
-    done(err);
+  server.register(plugins, (err) => {
+    if (err) { return done(err); }
+
+    server.start(done);
   });
-}
+};
