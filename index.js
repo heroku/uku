07fccce079ea4de3d3ae6df0024e8b354aea01df
Johnny Domino <jvdomino@gmail.com>
2015-06-19 11:36:54 -0700

    get that code coverage

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const Glue = require('glue');
 
-exports.run = function(manifest, options) {
+exports.run = function (manifest, options) {
 
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
 
-    return Glue.compose(manifest, options, function(err, server) {
-
-      if (err) {
-
-        return reject(err);
-      }
-
-      else {
-
-        return server.start(function() {
-          return resolve(server);
-        });
-      }
+    return Glue.compose(manifest, options, function (err, server) {
+      exports.ready(err, server, resolve, reject);
     });
   });
+};
+
+exports.ready = function (err, server, resolve, reject) {
+
+  if (err) {
+
+    return reject(err);
+  }
+
+  else {
+
+    return server.start(function () {
+      return resolve(server);
+    });
+  }
 };
--- test/server.js
+++ test/server.js
@@ -4,23 +4,30 @@ const Lab = require('lab');
 const expect = Code.expect;
 const lab = exports.lab = Lab.script();
 
-let server;
 
+
+
+const Uku = require('../index');
+const manifest = {
+  server: {},
+  connections: [{
+    port: '5000'
+  }],
+  plugins: []
+};
+let server;
 
 lab.experiment('server', function () {
 
-  lab.before(function(done) {
+  lab.before(function (done) {
 
-    require('glue').compose({}, {}, function(err, _server) {
-
-      if (err) { throw err; }
-
-      server = _server;
+    Uku.run(manifest, {}).then((newServer) => {
+      server = newServer;
       done();
     });
   });
 
-  lab.after(function (done) {
+  lab.afterEach(function (done) {
 
     server.stop(done);
   });
@@ -30,4 +37,15 @@ lab.experiment('server', function () {
     expect(server.info.port).to.be.a.number();
     server.start(done);
   });
+
+  lab.test('rejects when Glue returns an error', function (done) {
+
+    const err1 = new Error();
+    Uku.ready(err1, null, null, function(err2) {
+
+      expect(err1).to.equal(err2);
+      done();
+    })
+  });
+
 });
